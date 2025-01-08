54a905cbc706a9c7bba791d767608ee2b90e0d6c
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-01-08 15:17:28 -0800

    only show each warning at most 1x

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,4 +1,10 @@
 from llava.train.train import train
 
 if __name__ == "__main__":
+    import warnings
+
+    # Ignore warnings that are shown multiple times
+    warnings.simplefilter("once")
+    warnings.warn("Only showing each warning once")
+
     train()
