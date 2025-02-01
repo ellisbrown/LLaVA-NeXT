a10ac283840623d9215a30c4c34ff7a33b46d911
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-02-01 13:05:19 -0800

    bunch of q type exps 2/1

---
+++ scripts/ai2/exp/2025_02_01___qtypes.sh
@@ -0,0 +1,22 @@
+
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__mt1_desc.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__mt1_ct.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__mt1_rel_dir.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__mt1_rel_dist.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__mt1_temp.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_desc.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_ct.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_rel_dir.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_rel_dist.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_temp.sh
+
+
+
+
+
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_grouped.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt1_mixed.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt3_grouped.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt3_mixed.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt5_grouped.sh
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_01__vid_mt5_mixed.sh
