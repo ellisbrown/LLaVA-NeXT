404dec29d26b9bab3584eb15f9f9d49514c3caa7
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-02-04 23:00:09 -0800

    add ovmixin video exp

---
+++ scripts/ai2/exp/2025_02_05_ovmixin_50k/launch_vid.sh
@@ -0,0 +1,6 @@
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov0pct.sh
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov20pct.sh
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov40pct.sh
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov60pct.sh
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov80pct.sh
+bash scripts/ai2/launch.sh --script  scripts/ai2/exp/2025_02_05_ovmixin_50k/2025_02_05_vid_ovmixin_50k_ov100pct.sh
