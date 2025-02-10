b0a27e6700b3ed38223e97a6ad368dc84a5a5c74
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-02-10 14:38:20 -0800

    launch abs dist tests

---
+++ scripts/ai2/exp/2025_02_10___launch.sh
@@ -0,0 +1,4 @@
+bash scripts/ai2/launch.sh --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist_oe.sh &
+bash scripts/ai2/launch.sh --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist.sh &
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist_oe.sh &
+bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist.sh &
