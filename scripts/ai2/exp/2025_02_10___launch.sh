6e45b432e8d822959e75a3ad7d0236e6f4137777
Ellis Brown <ellis.l.brown.ii@gmail.com>
2025-02-10 23:01:47 -0800

    add feb 10 exps

--- scripts/ai2/exp/2025_02_10___launch.sh
+++ scripts/ai2/exp/2025_02_10___launch.sh
@@ -1,4 +1,13 @@
-bash scripts/ai2/launch.sh --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist_oe.sh &
-bash scripts/ai2/launch.sh --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist.sh &
-bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist_oe.sh &
-bash scripts/ai2/launch.sh --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist.sh &
+bash scripts/ai2/launch.sh --gpus 4 --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist_oe.sh &
+bash scripts/ai2/launch.sh --gpus 4 --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_abs_dist.sh &
+
+bash scripts/ai2/launch.sh --gpus 8 --clusters 80gb --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist_oe.sh &
+bash scripts/ai2/launch.sh --gpus 8 --clusters 80gb --script scripts/ai2/exp/2025_02_10__vid_mt1_abs_dist.sh &
+
+
+
+bash scripts/ai2/launch.sh --gpus 4 --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_long_size_est_oe.sh &
+bash scripts/ai2/launch.sh --gpus 4 --clusters all --script scripts/ai2/exp/2025_02_10__ov_mt1_long_size_est_mc.sh &
+
+bash scripts/ai2/launch.sh --gpus 8 --clusters 80gb --script scripts/ai2/exp/2025_02_10__vid_mt1_long_size_est_oe.sh &
+bash scripts/ai2/launch.sh --gpus 8 --clusters 80gb --script scripts/ai2/exp/2025_02_10__vid_mt1_long_size_est_mc.sh &
